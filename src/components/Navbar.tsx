deea5d8f7b0c5f4251f564554666626133e2f32a
Avinash Mahananda <32406268+Avin008@users.noreply.github.com>
2023-05-02 17:10:36 +0530

    added: input && todolist component

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,6 +1,16 @@
 const Navbar = () => {
   return (
-    <nav className="h-12 fixed left-0 right-0 top-0 border-b border-black"></nav>
+    <nav className="h-12 justify-between fixed left-0 right-0 px-6 flex items-center top-0 border-b border-black">
+      <div>
+        <span className="text-base font-bold">
+          TODO APP
+        </span>
+      </div>
+      <ul className="flex font-medium gap-5">
+        <li>LOGIN</li>
+        <li>SIGNUP</li>
+      </ul>
+    </nav>
   );
 };
 
