29443989c466884cf358ac249e24adfeb496974d
Avinash Mahananda <32406268+Avin008@users.noreply.github.com>
2023-05-02 17:48:29 +0530

    added: todo input handler

--- src/components/TodoInput.tsx
+++ src/components/TodoInput.tsx
@@ -1,10 +1,17 @@
+import { useState } from "react";
 const TodoInput = () => {
+  const [input, setInput] = useState({ text: "" });
+
   return (
     <div className="border flex gap-1 mt-20 border-black w-full h-12">
       <input
         className="w-[80%] h-full px-2"
         type="text"
         placeholder="Enter Item Name"
+        value={input.text}
+        onChange={(
+          e: React.ChangeEvent<HTMLInputElement>
+        ) => setInput({ text: e.target.value })}
       />
       <button className="w-[20%] text-sm font-medium border-l px-1">
         ADD ITEM
